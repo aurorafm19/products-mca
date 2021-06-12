3f1df6b95bad1f45571a2870ab9cc0aea2fd2f27
aurorafm19 <auroram19@gmail.com>
2021-06-12 13:53:54 +0200

    fix breadcrumb style

--- src/components/breadcrumbs.js
+++ src/components/breadcrumbs.js
@@ -1,12 +1,13 @@
 import React,{ Component } from 'react';
 import '../css/header.css';
+import {PRODUCT_DETAIL_BREADCRUMB} from '../constants';
 
 export default class Breadcrumbs extends Component {
     render() {
       return (
         <div className="breadcrumbs">
             <a className="breadcrumbs" href="/">Home</a>
-            {window.location.href.includes('product') ? <div className="breadcrumbs">product detail</div> : <div></div>}
+            {window.location.href.includes('product') ? <div className="breadcrumbs is-detail">> {PRODUCT_DETAIL_BREADCRUMB}</div> : <div></div>}
         </div>
       );
     }
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const NOT_INFO_AVAILABLE = 'not info available';
 export const NOT_SEARCH_INFO = 'Sorry, there are no results for your search!';
+export const PRODUCT_DETAIL_BREADCRUMB = 'product detail';
 export const HOUR_EXPIRATION = 3600000;
